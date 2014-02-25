85025aea524273ccd8d6280946eab45dc0d0a564
Alex Bunardzic <alexbunardzic@gmail.com>
2014-02-25 09:02:20 -0800

    Make User model with password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225001733) do
+ActiveRecord::Schema.define(version: 20140225164522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,5 +37,14 @@ ActiveRecord::Schema.define(version: 20140225001733) do
   end
 
   add_index "roles", ["project_id"], name: "index_roles_on_project_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "active",          default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
