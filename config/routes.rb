677f8bb0c385055f1c350116c0e694c9710fbfec
Alex Bunardzic <alexbunardzic@gmail.com>
2014-02-25 09:24:16 -0800

    Implemented sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Softblueprint::Application.routes.draw do
     resources :roles
   end
   
+  resources :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
